96753bc139db28b6c616c9e66f6848eb80bdfa4a
Erve Bayette <ErveBayette221@gmail.com>
2023-11-23 07:09:09 +0000

    - Add tasks with due dates and delete functionality - Improve data binding and UI updates

--- StudyGuide/MainPage.xaml.cs
+++ StudyGuide/MainPage.xaml.cs
@@ -1,36 +1,80 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
 
 namespace StudyGuide
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
-        public ObservableCollection<string> StudyItems { get; set; } = new ObservableCollection<string>();
+        public ObservableCollection<StudyTask> StudyItems { get; set; } = new ObservableCollection<StudyTask>();
 
         public MainPage()
         {
             InitializeComponent();
-            StudyItems.Add("Study Task 1");
-            StudyItems.Add("Study Task 2");
-            // Add more initial study tasks later
+            StudyItems.Add(new StudyTask { TaskName = "Study Task 1" });
+            StudyItems.Add(new StudyTask { TaskName = "Study Task 2" });
+            BindingContext = this; // Set BindingContext to the instance of MainPage
         }
-
-        
 
         private void OnAddTaskClicked(object sender, EventArgs e)
         {
-            // Get the new task from the Entry
             string newTask = NewTaskEntry.Text;
+            DateTime dueDate = DueDatePicker.Date;
 
-            // Add the new task to the ObservableCollection
-            StudyItems.Add(newTask);
+            StudyItems.Add(new StudyTask { TaskName = newTask, DueDate = dueDate });
 
-            // Clear the Entry for the next task
             NewTaskEntry.Text = "";
+            DueDatePicker.Date = DateTime.Now; // Reset DatePicker
+
+            OnPropertyChanged(nameof(StudyItems)); // Refresh the ListView
+        }
+    }
+
+    public class StudyTask : System.ComponentModel.INotifyPropertyChanged
+    {
+        private string taskName;
+        public string TaskName
+        {
+            get { return taskName; }
+            set
+            {
+                taskName = value;
+                OnPropertyChanged(nameof(TaskName));
+            }
+        }
+
+        private DateTime dueDate;
+        public DateTime DueDate
+        {
+            get { return dueDate; }
+            set
+            {
+                dueDate = value;
+                OnPropertyChanged(nameof(DueDate));
+            }
+        }
+
+        public ICommand DeleteCommand { get; set; }
+
+        public StudyTask()
+        {
+            DeleteCommand = new Command(OnDelete);
+        }
+
+        private void OnDelete()
+        {
+            // Access the StudyItems collection from the MainPage class
+            var mainPage = (MainPage)App.Current.MainPage;
+            mainPage.StudyItems.Remove(this);
+        }
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
         }
     }
 }
-
-
