860769876315fafbfcfe06773eca92dd33090e2a
Erve Bayette <ErveBayette221@gmail.com>
2023-11-23 00:03:53 +0000

    Add project files.

---
+++ StudyGuide/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StudyGuide
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ StudyGuide/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace StudyGuide
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public ObservableCollection<string> StudyItems { get; set; } = new ObservableCollection<string>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            StudyItems.Add("Study Task 1");
+            StudyItems.Add("Study Task 2");
+            // Add more initial study tasks as needed
+        }
+
+        
+    }
+}
+
