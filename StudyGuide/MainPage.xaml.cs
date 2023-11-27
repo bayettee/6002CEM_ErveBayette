473839063de8fa1cfddebe2bff381010064b0f13
Erve Bayette <ErveBayette221@gmail.com>
2023-11-27 07:54:36 +0000

    Add due date for each study task in MainPage.xaml.cs Modified MainPage.xaml.cs to include a due date for each study task. The due date is displayed in the ListView, and the code has been updated to handle the due date property for each task.

--- StudyGuide/MainPage.xaml.cs
+++ StudyGuide/MainPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.ObjectModel;
 using System.Linq;
 using Microsoft.Maui.Controls;
@@ -15,24 +14,38 @@ namespace StudyGuide
         public MainPage()
         {
             InitializeComponent();
-            StudyItems.Add(new StudyTask { TaskName = "Study Task 1", Priority = "High" });
-            StudyItems.Add(new StudyTask { TaskName = "Study Task 2", Priority = "Medium" });
+            StudyItems.Add(new StudyTask { TaskName = "Study Task 1", Priority = "High", DueDate = DateTime.Now, Subject = "Math", Topic = "Algebra", Notes = "Practice solving equations" });
+            StudyItems.Add(new StudyTask { TaskName = "Study Task 2", Priority = "Medium", DueDate = DateTime.Now.AddDays(2), Subject = "Science", Topic = "Chemistry", Notes = "Review periodic table" });
             BindingContext = this; // Set BindingContext to the instance of MainPage
         }
 
-      
+        
 
         private void OnAddTaskClicked(object sender, EventArgs e)
         {
             string newTask = NewTaskEntry.Text;
             DateTime dueDate = DueDatePicker.Date;
             string priority = PriorityPicker.SelectedItem as string;
+            string subject = SubjectEntry.Text;
+            string topic = TopicEntry.Text;
+            string notes = NotesEditor.Text;
 
-            StudyItems.Add(new StudyTask { TaskName = newTask, DueDate = dueDate, Priority = priority });
+            StudyItems.Add(new StudyTask
+            {
+                TaskName = newTask,
+                DueDate = dueDate,
+                Priority = priority,
+                Subject = subject,
+                Topic = topic,
+                Notes = notes
+            });
 
             NewTaskEntry.Text = "";
             DueDatePicker.Date = DateTime.Now; // Reset DatePicker
             PriorityPicker.SelectedIndex = -1; // Reset PriorityPicker
+            SubjectEntry.Text = "";
+            TopicEntry.Text = "";
+            NotesEditor.Text = "";
 
             OnPropertyChanged(nameof(StudyItems)); // Refresh the ListView
         }
@@ -57,53 +70,81 @@ namespace StudyGuide
             StudyItems = new ObservableCollection<StudyTask>(StudyItems.OrderBy(task => task.Priority));
             OnPropertyChanged(nameof(StudyItems)); // Refresh the ListView
         }
-    }
 
-    public class StudyTask : System.ComponentModel.INotifyPropertyChanged
-    {
-        private string taskName;
-        public string TaskName
+        public class StudyTask : System.ComponentModel.INotifyPropertyChanged
         {
-            get { return taskName; }
-            set
+            private string taskName;
+            public string TaskName
             {
-                taskName = value;
-                OnPropertyChanged(nameof(TaskName));
+                get { return taskName; }
+                set
+                {
+                    taskName = value;
+                    OnPropertyChanged(nameof(TaskName));
+                }
             }
-        }
 
-        private DateTime dueDate;
-        public DateTime DueDate
-        {
-            get { return dueDate; }
-            set
+            private DateTime dueDate;
+            public DateTime DueDate
             {
-                dueDate = value;
-                OnPropertyChanged(nameof(DueDate));
+                get { return dueDate; }
+                set
+                {
+                    dueDate = value;
+                    OnPropertyChanged(nameof(DueDate));
+                }
             }
-        }
 
-        private string priority;
-        public string Priority
-        {
-            get { return priority; }
-            set
+            private string priority;
+            public string Priority
             {
-                priority = value;
-                OnPropertyChanged(nameof(Priority));
+                get { return priority; }
+                set
+                {
+                    priority = value;
+                    OnPropertyChanged(nameof(Priority));
+                }
             }
-        }
 
-        public StudyTask()
-        {
-            
-        }
+            private string subject;
+            public string Subject
+            {
+                get { return subject; }
+                set
+                {
+                    subject = value;
+                    OnPropertyChanged(nameof(Subject));
+                }
+            }
 
-        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+            private string topic;
+            public string Topic
+            {
+                get { return topic; }
+                set
+                {
+                    topic = value;
+                    OnPropertyChanged(nameof(Topic));
+                }
+            }
 
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+            private string notes;
+            public string Notes
+            {
+                get { return notes; }
+                set
+                {
+                    notes = value;
+                    OnPropertyChanged(nameof(Notes));
+                }
+            }
+
+            public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
